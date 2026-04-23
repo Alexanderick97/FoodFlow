edb2b3e69da00acb2f1e45957c16f2ffe49b24c8
VA- Alumno <VA-Alumno@duoc.acad>
2026-04-23 18:44:13 -0400

    feat:Erick. agregar estructura de mis 5 microservicios

---
+++ restaurante-ms/src/main/java/com/CodeChefs/restaurante_ms/RestauranteMsApplication.java
@@ -0,0 +1,13 @@
+package com.CodeChefs.restaurante_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteMsApplication.class, args);
+	}
+
+}
