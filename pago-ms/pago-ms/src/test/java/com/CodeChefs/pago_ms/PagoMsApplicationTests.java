f61f613266ea9342d624ef295128fe1e978b3f20
VA-Alumno <er.gonzalezv@duocuc.cl>
2026-05-04 16:56:27 -0400

    Estructura de MS's de Cristofer Pago y Orden

---
+++ pago-ms/pago-ms/src/test/java/com/CodeChefs/pago_ms/PagoMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CodeChefs.pago_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagoMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
