4b1c452b2f78383c3561c62b2f257f9253f8d406
VA-Alumno <er.gonzalezv@duocuc.cl>
2026-05-04 17:33:50 -0400

    Pom.xml y application.properties de MS's de cristofer

---
+++ promocion-ms/src/main/java/com/CodeChefs/promocion_ms/PromocionMsApplication.java
@@ -0,0 +1,13 @@
+package com.CodeChefs.promocion_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromocionMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromocionMsApplication.class, args);
+	}
+
+}
