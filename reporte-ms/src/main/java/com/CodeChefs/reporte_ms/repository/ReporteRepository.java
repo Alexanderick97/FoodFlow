e2d4de6fbacde72afc2e0bb0ab1c724adfc1ad57
cristo767 <cr.pizarroh@duocuc.cl>
2026-05-17 22:48:06 -0400

    Update ReporteRepository.java

--- reporte-ms/src/main/java/com/CodeChefs/reporte_ms/repository/ReporteRepository.java
+++ reporte-ms/src/main/java/com/CodeChefs/reporte_ms/repository/ReporteRepository.java
@@ -1,6 +1,6 @@
-package com.CodeChefs.ReporteMS.repository;
+package com.CodeChefs.reporte_ms.repository;
 
-import com.CodeChefs.ReporteMS.model.Reporte;
+import com.CodeChefs.reporte_ms.model.Reporte;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
