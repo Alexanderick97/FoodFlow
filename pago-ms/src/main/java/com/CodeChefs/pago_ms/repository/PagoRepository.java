7f2b6bd827b20cb4b146ef478ba231f7602f3a06
cristo767 <cr.pizarroh@duocuc.cl>
2026-06-21 00:44:28 -0400

    Update PagoRepository.java

--- pago-ms/src/main/java/com/CodeChefs/pago_ms/repository/PagoRepository.java
+++ pago-ms/src/main/java/com/CodeChefs/pago_ms/repository/PagoRepository.java
@@ -2,7 +2,9 @@ package com.CodeChefs.PagoMS.repository;
 
 import com.CodeChefs.PagoMS.model.Pago;
 import org.SpringFramework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface PagoRepository extends JpaRepository<Pago, long> {
+  Optional<Pago> findByIdOrden(Long idOrden);
 
 }
